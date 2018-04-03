0f13d1a96bd4cbab807f573b8d82d70bccbc2fba
Andrew Kemp <akemp850@gmail.com>
2018-04-03 16:10:44 +0100

    Sinatra exercise - Views step

---
+++ app.rb
@@ -0,0 +1,23 @@
+require 'sinatra'
+require 'shotgun'
+set :session_secret, 'super secret'
+
+get '/' do
+  'Hello!'
+end
+
+get '/secret' do
+  'Today is Tuesday 3rd April 2018'
+end
+
+get '/next' do
+  'I am working with Shih Han today'
+end
+
+get '/last' do
+  'Tomorrow is Wednesday'
+end
+
+get '/cat' do
+  '<img src=http://bit.ly/1eze8aE style="border: dashed red">'
+end
